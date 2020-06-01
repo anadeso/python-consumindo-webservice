408bf8cbc83b84572d4f3de2fd9de1827edbdb29
Ana Sousa <anadeso90@gmail.com>
2020-05-31 22:27:09 -0300

    commit ab

--- soap.py
+++ soap.py
@@ -1,6 +1,6 @@
 from zeep import Client
 
 client = Client('http://soapclient.com/cxml/soapresponder.wsdl')
-result = client.service.Method1('oi',  'tchau')
+result = client.service.Method1('oi', 'tchau')
 
 print(result) 
